e31627cbd5dd365f31f1f1354794356992394098
QMels <qjm7@nau.edu>
2021-02-19 12:32:02 -0700

    textbot tests

--- website/textbot/text_chat.py
+++ website/textbot/text_chat.py
@@ -1,14 +1,32 @@
 # imports
 from flask import Flask, request
 import requests
+from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
+# twilio acc info
+account_sid = "AC16136c6a424ad7503d7df3a3bdb2a422"
+auth_token = "9a037326814f2b00f9ede3e76d2792a2"
+account_num = "+13852360797"
+
+client = Client(account_sid, auth_token)
+
+# Flask setup
+app = Flask(__name__)
+
+@app.route('/textbot')
+def hello_world():
+    return 'Test'
+
+if __name__ == "__main__":
+	app.run(debug = True)
+
+"""
 # database placeholder
 databaseHolder = { 'qMelssen' : 'twitter: qmelTwit\n facebook: qmelFace',
 				   'maxMose' : 'twitter: MoseTwit\n facebook: MoseFace' }
 
-# Flask setup
-app = Flask(__name__)
+
 
 # routing POSTs to file
 @app.route('/textbot', methods=['POST'])
@@ -31,3 +49,8 @@ def bot():
 		responded = True
 
 	return str(resp)
+
+def generateText():
+	format = request.args.get('test')
+
+"""
