0ab358395d1f560f683271f0e1483f8aae94ca2b
unknown <cartertaylorcs@gmail.com>
2021-03-22 21:18:24 -0700

    adds function for determing if form filled out completely

--- website/database/signup.js
+++ website/database/signup.js
@@ -1,15 +1,97 @@
 //var mysql = require ('mysql');
-//import mysql from 'mysql';
-// const mysql = require("mysql")
-// mysql.func()
 
 
-// var connection = mysql.createConnection({
-//   host: '',
-//   user: '',
-//   password: '',
-//   database: ''
-// });
+/* Function: loginBoxesValid
+   Algorithm: searches object of log in info to make sure all boxes were properly filled out
+    if not, the user will be asked to fill in the info
+   Precondition: an object of key value pairs, each key representing an input box (ex: username)
+    and each value being what was submitted in the box
+   Postcondition: True, if all boxes were filled out, False if an empty box was found 
+*/
+function loginBoxesValid(signInInfo)
+{
+  // iterate over boxes
+  for (value in signInInfo)
+  {
+    // check for empty box 
+    if (signInInfo[value] == "")
+    {
+      return false;
+    }
+
+  }
+
+  // otherwise no empty box was found
+  return true;
+}
+
+
+/* Function: createAccount()
+   Algorithm:  
+   Precondition: is called when the "Create Account" button is clicked
+   Postcondition: Creates user account if 
+    1. All boxes are filled in 
+    2. Username is not taken
+    Otherwise: No submission will be made, user will be notified of the error 
+*/
+async function createAccount()
+{
+
+  // Gather variable information from HTML form boxes
+  let userName = document.getElementById("validationDefaultUsername").value;
+  let firstName = document.getElementById("validateFirstName").value;
+  let lastName = document.getElementById("validateLastName").value;
+  let phoneNumber = document.getElementById("validatePhoneNumber").value;
+  let email = document.getElementById("validateEmail").value;
+  
+
+  // create object to be sent in post request
+  const signInInfo = {firstName, lastName, phoneNumber, email, userName};
+    
+  // check for all boxes filled in for sign up 
+  if (loginBoxesValid(signInInfo))
+  {
+    
+    // contains information regarding the type of fetch request we are doing
+    const options = {
+      method: 'POST', // type of data 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(signInInfo) // the actual location(info) passed into the post request as JSON strings
+    }
+    
+    console.log(signInInfo);
+
+    // actual fetch (post) request (sends the data over)
+      // response is what is sent back (we can configure this in server.js)
+    const response = await fetch('/signup', options) 
+    const responseData = await response.json();
+    console.log(responseData);
+    console.log(responseData.databaseResponse);
+    
+    // check for if the user was created (if username is already taken)
+    if (responseData.userCreated == false)
+    {
+      window.alert("Sorry, that username was alread taken, please try with another")
+    }
+
+  }
+
+  /*
+  var set = {userName: }
+  var sql = "INSERT INTO test_key SET " + user_name;
+
+  var query = connection.query(sql, function(error, results, fields) {
+    if (error) window.alert("Username taken");
+  });
+
+  connection.end();
+  */
+
+}
+
+
 
 //window.alert("this is working!")
 // connection.connect();
@@ -81,58 +163,5 @@ console.log(query.sql);
 
 // connection.end();
 
-function createAccount()
-{
-/*
-  var connection = mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
-  });
-
-  connection.connect();
-*/
-
-  let firstName = document.getElementById("validateFirstName").value + "";
-  let lastName = document.getElementById("validateLastName").value;
-  let phoneNumber = document.getElementById("validatePhoneNumber").value;
-  let email = document.getElementById("validateEmail").value;
-  let userName = document.getElementById("validationDefaultUsername").value;
-
-  // create object to be sent in post request
-  const signInInfo = {firstName, lastName, phoneNumber, email, userName};
-
-  console.log(signInInfo)
-
-  // contains information regarding the type of fetch request we are doing
-  const options = {
-    method: 'POST', // type of data 
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body:JSON.stringify(signInInfo) // the actual location(info) passed into the post request
-  }
-  
-  console.log(signInInfo);
-
-  // actual fetch (post) request (sends the data over)
-    // response is what is sent back (we can configure this in server.js)
-  fetch('../signup', options).then (response=> {
-    console.log(response.json());
-  });
 
 
-
-  /*
-  var set = {userName: }
-  var sql = "INSERT INTO test_key SET " + user_name;
-
-  var query = connection.query(sql, function(error, results, fields) {
-    if (error) window.alert("Username taken");
-  });
-
-  connection.end();
-  */
-
-}
