1850a4c6de8018865862eb68a0eb51d3b957907a
Carter Taylor <cartertaylorcs@gmail.com>
2021-04-11 16:50:48 -0700

    begins work on providing user login session

---
+++ website/passport-config.js
@@ -0,0 +1,56 @@
+const LocalStrategy = require('passport-local')
+
+function initialize(passport)
+{
+ 
+    passport.use(new LocalStrategy(function
+      
+        // username and password provided from login
+          // authCheckDone is a callback that checks for pass or failed connection
+        (username, password, authCheckDone)
+          {
+            
+            // this part will be done with sql query (https://gist.github.com/manjeshpv/84446e6aa5b3689e8b84)
+            console.log("yoooooooooooooooooooo")
+            console.log(username + "" + password)
+            // check for userName 
+            if (username != "dogMan2223")
+            {
+              // function callback for not found
+              return authCheckDone(null, false, {message:"Incorrect username"});
+            }
+      
+            // check for password (use sample bycrpyt code from discord)
+            if (password != "words123")
+            {
+              // function callback for not found
+              return authCheckDone(null, false, {message:"Incorrect password"});
+            }
+            console.log("did we get here?")
+      
+            // otherewise we validated the user
+              // maybe use result.inserID from query of user, which could make username
+              // an object with an id next to it 
+            return authCheckDone(null, username);
+          }
+        
+      ));
+      
+      // allows you to persist data accross session
+      passport.serializeUser((user, done) =>
+        {
+          console.log("user provided: " + user._id);
+          done(null, user);
+        }
+      );
+      
+      passport.deserializeUser((id, done)=> 
+        {
+          console.log("id provided: " + id);
+          done(null,{id});
+        }
+      );
+}
+
+// export file (function initialize)
+module.exports = initialize;
--- website/routes/home.js
+++ website/routes/home.js
@@ -1,15 +1,96 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+// helps us log out 
+const methodOverride = require('method-override');
 
+router.use(methodOverride('_method'));
 
-router.get('/bobby', function(req, res, next) {
-    res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
-  });
+/* GET home page. */
+router.get('/', checkNotAuthenticated, function(req, res, next) {
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
+});
 
-router.post('/test/submit', function (req, res, next)
+router.post('/submit', checkNotAuthenticated, passport.authenticate('local',
 {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+  // on succes, go to user page
+  // on failure, go back to login / homepage
+
+  successRedirect:'/test',
+  failureRedirect:'/',
+  failureFlash: true
+  // check validity of values
+ // req.check('email', 'invalid email address').isEmail();
+  //req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  // IF NOT VALID (user or password not found)
+    
+  
+    // console.log(req.body)
+    // console.log(req.session)
+
+    // var errors = req.validationErrors();
+    // if (errors)
+    // {
+    //   req.session.errors = errors;
+    //   req.session.success = false;
+    // }
+    // else
+    // {
+    //   req.session.success = true;
+    //   res.redirect('/test')
+    // }
+  
+  // otherwise we redirect to the user page 
+  // res.redirect('/test');
+
+}));
+
+ensureAuthenticated = (req, res, next) =>
+{
+  console.log("checking authentication");
+  if (req.isAuthenticated())
+  {
+    console.log("WE ARE ATHENTICATED")
+    // /test will move to the next function which renders the page
+    return next()
+  }
+  console.log("did this run?")
+  // otherwise we redirect to the user page
+  res.redirect('/')
+  
+}
+
+// if the user is authenticated, then they should not be able  to go to log in
+function checkNotAuthenticated  (req, res, next )
+{
+  if (req.isAuthenticated())
+  {
+    // redirect to the users unique page
+    res.redirect('/test')
+  }
+
+  // otherwise continue with the nect function
+  next();
+}
+
+// will be the userpage
+router.get('/test', ensureAuthenticated, function(req, res, next) {
+  res.render('test', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  // req.session.errors = null;
+});
+
+
+// for logging out
+router.delete('/logout', (req, res) =>
+{
+  // passport provided method to delete session
+  req.logOut()
+  // resdiret user back to the home page
+  res.redirect('/')
 })
+
+
 
 module.exports = router;
