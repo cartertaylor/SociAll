1ffb200ef8ad312ea546c9eccc343fb32fbabd21
Carter Taylor <cartertaylorcs@gmail.com>
2021-04-11 19:15:44 -0700

    correctly validates user login with existing users in database

--- website/passport-config.js
+++ website/passport-config.js
@@ -1,4 +1,31 @@
 const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs');
+const mysql = require('mysql');
+require('dotenv').config()
+
+// grabbing env variables for database
+const databaseHost = process.env.DATABASE_HOST_NAME;
+const databaseUser = process.env.DATABASE_USER_NAME;
+const databasePassword = process.env.DATABASE_PASSWORD ;
+const databaseName = process.env.DATABASE_NAME;
+const userTable = process.env.DATABASE_MAIN_TABLE;
+
+var connection = mysql.createConnection({
+  host: databaseHost,
+  user: databaseUser,
+  password: databasePassword,
+  database: databaseName
+}, 'pool');
+
+// TEST CONNECTION
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
+});
+
+
+
+
 
 function initialize(passport)
 {
@@ -9,24 +36,59 @@ function initialize(passport)
           // authCheckDone is a callback that checks for pass or failed connection
         (username, password, authCheckDone)
           {
-            
-            // this part will be done with sql query, ref / help: (https://gist.github.com/manjeshpv/84446e6aa5b3689e8b84)
-            console.log("yoooooooooooooooooooo")
+            // Flags for confirming the username and password 
+            foundUsername = false;
+            foundPassword = false;
 
-            // check for userName 
-            if (username != "dogMan2223")
-            {
-              // function callback for not found
-              return authCheckDone(null, false, {message:"Incorrect username"});
-            }
+            // Looking for an existing username 
+            sql = mysql.format ("SELECT userName FROM ?? WHERE userName like ?", [userTable, username]);
+
+            connection.query (
+              sql, function (err, result, fields)
+              {
+                  if (err) throw err;
+
+                  // check if our query was able to find a user
+                  if (result[0].userName === username)
+                  {
+                    console.log("found user!")
+                    
+                    // if so, set the found flag
+                    foundUsername = true;
+                  }
+
+                  // if we didnt find the username, error
+                  if (!foundUsername)
+                  {
+                    // function callback for not found
+                    return authCheckDone(null, false, {message:"Incorrect username"});
+                  }
+              });
+
+
+            // look for existing password
+            sql = mysql.format ("SELECT password FROM ?? WHERE userName = ?", [userTable, username]);
+
+            connection.query (
+              sql, function (err, result, fields)
+              {
+                  if (err) throw err;
+                  
+                  // check hashedPassword against entered password
+                  if (bcrypt.compareSync(password, result[0].password))
+                  {
+                    foundPassword = true;
+                  }
+
+                  // if we didnt find the password, error
+                  if (!foundPassword)
+                  {
+                    // function callback for not found
+                    return authCheckDone(null, false, {message:"Incorrect password"});
+                  }
+
+              });
       
-            // check for password (use sample bycrpyt code from discord)
-            if (password != "words123")
-            {
-              // function callback for not found
-              return authCheckDone(null, false, {message:"Incorrect password"});
-            }
-            console.log("did we get here?")
       
             // otherewise we validated the user
               // maybe use result.inserID from query of user, which could make username
